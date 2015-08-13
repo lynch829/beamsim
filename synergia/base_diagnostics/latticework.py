1a3992737fa8a58cb450c3bae6a00fd1df05fe9a
ncook882 <ncook@radiasoft.net>
2015-08-13 14:19:26 -0600

    Added feature for extracting R-matrices from lattice elements

--- synergia/base_diagnostics/latticework.py
+++ synergia/base_diagnostics/latticework.py
@@ -2,6 +2,7 @@ import sys, os
 import synergia
 from mpi4py import MPI
 import synergia_workflow
+import numpy as np
 
 def make_chef(lattice, lattice_simulator):
     '''Make all elements of a lattice chef-propogated and update the lattice simulator.'''
@@ -111,3 +112,59 @@ def print_strengths(elemslist, unique=True):
                 print elem.get_name() + ' K2: ' + str(elem.get_double_attribute("k2"))             
 
 
+def get_maps(lattice):
+    '''Return the matrices for each step in the stepper'''
+    
+    for element in lattice.get_elements():
+        element.set_string_attribute("extractor_type", "chef_map")
+    map_order = 1
+    lattice_simulator = synergia.simulation.Lattice_simulator(lattice,
+                                                              map_order)
+    steps_per_element = 1
+    stepper = synergia.simulation.Independent_stepper_elements(lattice_simulator,
+                                                                steps_per_element)
+    
+    maps = []
+    
+    stepper.force_update_operations_no_collective()
+    for step in stepper.get_steps():
+        for operator in step.get_operators():
+            if operator.get_type() == 'independent':
+                io = synergia.simulation.as_independent_operator(operator)
+                slices = io.get_slices()
+                #print "%s:" % slices[0].get_lattice_element().get_name()
+                if len(slices) > 1:
+                    raise RuntimeError, "found multiple slices in an independent operator"
+                for operation in io.get_operations():
+                    if operation.get_type() == 'fast_mapping':
+                        fmo = synergia.simulation.as_fast_mapping_operation(operation)
+                        dense_mapping = synergia.simulation.Dense_mapping(fmo.get_fast_mapping())
+                        #print "order 0:"
+                        #print dense_mapping.get_constant_term()
+                        #print "order 1:"
+                        #print dense_mapping.get_linear_term()
+                        #etype = io.get_slices()[0].get_lattice_element().get_type()
+                        #ename = io.get_slices()[0].get_lattice_element().get_name()
+                        dmap = dense_mapping.get_linear_term()
+                        dmap_copy = np.copy(dmap)
+                        #dmmap = dense_mapping.get_linear_term_mad()                        
+                        maps.append(np.asarray(dmap_copy))
+                        #if etype == 'quadrupole':
+                            #dmap = dense_mapping.get_linear_term()
+                            #dmap_copy = np.copy(dmap)
+                            #dmmap = dense_mapping.get_linear_term_mad()
+                            #print ename + ":"
+                            #print dmap
+                            #if dmap.all() == dmmap.all():
+                                #print "mad map is same"
+                            #maps.append(np.asarray(dmap_copy))
+    return maps
+    
+def combine_maps(maplist):
+    copylist = list(maplist) #copy the list to preserve the original
+    dim = copylist[0].shape[0]
+    final = np.identity(dim)
+    while len(copylist) > 0:
+        mult = copylist.pop()
+        final = np.dot(mult,final)
+    return final
